c0feb043f88699fb32d7db6c6d83e67bdb252f21
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-05-06 08:22:08 +0530

    Implemented insert and get operations in Employee controller

--- employee-api/src/main/java/com/eternity/employeeapi/controller/EmployeeController.java
+++ employee-api/src/main/java/com/eternity/employeeapi/controller/EmployeeController.java
@@ -5,10 +5,8 @@ import com.eternity.employeeapi.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,21 +15,29 @@ public class EmployeeController {
     @Autowired
     private IEmployeeService service;
 
-    //Save
     @PostMapping("/save")
     public ResponseEntity<String> saveEmployee(@RequestBody Employee employee) {
         Integer id = service.saveEmployee(employee);
-        return new ResponseEntity<String>("Employee '"+id+" 'saved", HttpStatus.OK);
+        return new ResponseEntity<String>(
+                "Employee '"+id+" 'saved",
+                HttpStatus.OK
+        );
     }
 
-    //Fetch all
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployee() {
+        List<Employee> list = service.getAllEmployees();
+        return new ResponseEntity<>(
+                list,
+                HttpStatus.OK
+        );
+    }
 
-
-    //Fetch one
-
-
-    //Delete
-
+    @GetMapping("/one/{id}")
+    public ResponseEntity<Employee> getOneEmployee(@PathVariable Integer id) {
+        Employee employee = service.getOneEmployee(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
 
     //Update
 
