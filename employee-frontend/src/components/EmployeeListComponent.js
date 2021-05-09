18bcf559ad61e14979300a661f354a6dea8b33f2
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-05-10 00:31:36 +0530

    Implemented list employees

--- employee-frontend/src/components/EmployeeListComponent.js
+++ employee-frontend/src/components/EmployeeListComponent.js
@@ -1,11 +1,54 @@
 import React from "react";
+import EmployeeService from "../services/EmployeeService";
 
 
 export class EmployeeListComponent extends React.Component {
-        render() {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            employees: []
+        }
+    }
+    
+    componentDidMount() {
+        this.getAllEmployees();
+    }
+
+    getAllEmployees() {
+        //Make service call --> get data --> Set data to state(employees)
+        EmployeeService.getAllEmployees().then((response)=>{
+            this.setState({ employees:response.data });
+            console.log('Response: ' + this.state.employees[0].name);
+        });
+    }
+
+    render() {
             return(
                 <div>
-                    HELLO LIST COMPOENENT
+                    <h2>EMPLOYEE LIST VIEW</h2>
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>SALARY</th>
+                                <th>DEPARTMENT</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.employees.map(emp=>
+                                <tr key={emp.id}>
+                                    <td>{emp.id}</td>
+                                    <td>{emp.name}</td>
+                                    <td>{emp.salary}</td>
+                                    <td>{emp.department}</td>
+                                </tr>
+                            )
+                        }
+                        </tbody>
+                    </table>
                 </div>
             )
         }
--- employee-frontend/src/services/EmployeeService.js
+++ employee-frontend/src/services/EmployeeService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const URL = "http://localhost:8081/employees/" 
+const URL = "http://localhost:8081/employees" 
 
 class EmployeeService {
 
     //1. Fetch All employees JSON Data
-    getAllEmployee() {
-        axios.get(`${URL}/all`);
+    getAllEmployees() {
+        return axios.get(`${URL}/all`);
     }
 
 }
