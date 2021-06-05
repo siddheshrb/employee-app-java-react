6fcc5e7c226a142af326bfe9cbd1dc0682b7d7fa
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-06-05 18:03:18 +0530

    Designed add employee page

--- employee-frontend/src/components/CreateEmployeeComponent.js
+++ employee-frontend/src/components/CreateEmployeeComponent.js
@@ -6,14 +6,77 @@ class CreateEmployeeComponent extends React.Component {
         super(props);
 
         this.state = {
-
+            name: '',
+            salary: '',
+            department: ''
         }
+
+        this.changeNameHandler = this.changeNameHandler.bind(this);
+        this.changeSalaryHandler = this.changeSalaryHandler.bind(this);
+        this.changeDepartmentHandler = this.changeDepartmentHandler.bind(this);
+        this.saveEmployee = this.saveEmployee.bind(this);
+    }
+
+    changeNameHandler = (event) => {
+        this.setState({name: event.target.value});
+    }
+
+    changeSalaryHandler = (event) => {
+        this.setState({salary: event.target.value});
+    }
+
+    changeDepartmentHandler = (event) => {
+        this.setState({department: event.target.value});
+    }
+
+    saveEmployee = (e) => {
+        e.preventDefault();
+
+        let employee = {
+            name: this.state.name,
+            salary: this.state.salary,
+            department: this.state.department
+        };
+        console.log('employee => ' + JSON.stringify(employee));
+    }
+
+    cancel() {
+        this.props.history.push('/employees');
     }
 
     render() {
         return(
             <div>
-                <h1>Employee Form</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3 className="text-center">Add Employee</h3>
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label>Name</label>
+                                        <input placeholder="Name" name="name" className="form-control"
+                                               value={this.state.name} onChange={this.changeNameHandler}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Salary</label>
+                                        <input placeholder="Salary" name="salary" className="form-control"
+                                               value={this.state.salary} onChange={this.changeSalaryHandler}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Department</label>
+                                        <input placeholder="Department" name="department" className="form-control"
+                                               value={this.state.department} onChange={this.changeDepartmentHandler}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <button className = "btn btn-success" onClick={this.saveEmployee}>Save</button>
+                                        <button className = "btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
